5b72827732eaed61dd9b5e41937da2727046538d
Michal Zimmermann <zimmicz@gmail.com>
2019-08-09 15:17:08 +0200

    Add marker icon

--- example/map.js
+++ example/map.js
@@ -1,7 +1,7 @@
 var mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, }).addTo(mymap);
 var myIcon = L.icon({
-    iconUrl: '../node_modules/leaflet/dist/images/marker-icon.png',
+    iconUrl: '../img/map-marker.svg',
     iconRetinaUrl: 'marker-icon-2x.png',
     shadowUrl:     'marker-shadow.png',
     iconSize:    [25, 41],
